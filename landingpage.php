9909dad68cc92cf56e965ba2409e5565964336c8
Yoshiyuki Canoy <canoy.yoshiyuki@gmail.com>
2023-07-25 14:41:33 +0800

    fix landing page connceted na sa admin user login

---
+++ landingpage.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./src/css/custom.css">
+    <link rel="stylesheet" href="./src/css/bootstrap.min.css">
+
+    <script defer src="./src/js/bootstrap.bundle.min.js"></script>
+</head>
+
+
+
+<body>
+
+  <!-- -->
+
+ 
+
+
+<nav class="navbar navbar-expand-lg border-bottom border-black navbar-light">
+    <div class="container">
+        <a href="#" class="navbar-brand">
+            <img src="./src/img/tupvlogo.png" height="70" alt="CoolBrand">
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarCollapse">
+            <div class="navbar-nav tupv ">
+                <a href="http://www.tupvisayas.edu.ph/" class="nav-item nav-link"> <span>T</span>echnological <span>U</span>niversity of the <span>P</span>hilippines <span>V</span>isayas</a>
+           
+            </div>
+            <div class="navbar-nav ms-auto login">
+                <a href="./user/user_login.php" class="nav-item nav-link  p-3 m-1">FACULTY</a>
+                <a href="./admin/admin_login.php" class="nav-item nav-link p-3 m-1">ADMIN</a>
+          
+            </div>
+        </div>
+    </div>
+</nav>      
+
+<section class="main">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-7">
+                <h1>TUPV</h1>
+                <h1>E-LEARNING</h1>
+                <h1>SYSTEM</h1>
+                <button>.</button>
+                <h5>Powered by College of Engineering</h5>
+            </div>
+        </div>
+    </div>
+
+
+    <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,32L80,74.7C160,117,320,203,480,213.3C640,224,800,160,960,117.3C1120,75,1280,53,1360,42.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+</section>
+
+<section class="about my-5 py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center fw-bold"><span>L</span>orem <span>I</span>psum</h1>
+                <p class="justify-content-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae, totam incidunt odit consectetur animi sunt tempora impedit repudiandae, illo excepturi labore similique quis, ipsum voluptatum a corporis repellat. Ipsam nam, quas sunt adipisci vitae omnis. Facere aliquam nihil odio voluptas ipsam? Fugiat nostrum maxime quam ullam dolorem odit nam eius? Ipsa aspernatur, nostrum ad iure et consequatur eveniet natus. Cum quis, sint inventore possimus odio modi minus explicabo animi?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae, totam incidunt odit consectetur animi sunt tempora impedit repudiandae, illo excepturi labore similique quis, ipsum voluptatum a corporis repellat. Ipsam nam, quas sunt adipisci vitae omnis. Facere aliquam nihil odio voluptas ipsam? Fugiat nostrum maxime quam ullam dolorem odit nam eius? Ipsa aspernatur, nostrum ad iure et consequatur eveniet natus. Cum quis, sint inventore possimus odio modi minus explicabo animi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae, totam incidunt odit consectetur animi sunt tempora impedit repudiandae, illo excepturi labore similique quis, ipsum voluptatum a corporis repellat. Ipsam nam, quas sunt adipisci vitae omnis. Facere aliquam nihil odio voluptas ipsam? Fugiat nostrum maxime quam ullam dolorem odit nam eius? Ipsa aspernatur, nostrum ad iure et consequatur eveniet natus. Cum quis, sint inventore possimus odio modi minus explicabo animi?</p>
+        </div>
+        </div>
+    </div>
+
+
+</section>
+
+
+
+<section class="footer">
+    <div class="container">
+    <div class="row py-5 sm-center d-flex justify-content-center">
+        <div class="col-lg-5 py-5"> 
+            <h5 class="fw-bold lh-1">TUPV E-LEARNING SYSTEM</h5>      
+            <button></button> 
+            <p>Powered by College of Engineering</p>
+        </div>
+        <div class="col-lg-4 py-5">
+        <img src="./src/img/tupvlogo.png" height="160" alt="CoolBrand">
+        </div>
+        <div class="col-lg-3 py-5">
+            <h4>Address</h4>
+            <h6>Talisay Campus Capitan Sabi Street,Brgy. Zone 12 Talisay City,Negros Occidental</h6>
+            <h4>Contact</h4>
+            <h6>(034) 495 3480</h6>
+        </div>
+    </div>
+    </div>
+</section>
+
+</body>
+</html>
